30bdc2592fd1c0f7f417ea07b5f1ec4eaea9cfec
くり太郎 <62167170+kuritaro1122@users.noreply.github.com>
2021-12-29 09:23:56 +0900

    Add files via upload

---
+++ CatmullRomTester.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using JPBotelho;
+
+namespace Curve.Tester {
+    [AddComponentMenu(CurveTesterPath + "CatmullRomTester")]
+    public class CatmullRomTester : BaseCurveTester {
+        [SerializeField, Min(2)] int resolution = 1;
+        [SerializeField] bool closeLoop = false;
+        private void OnValidate() => base.SetCurve(new CatmullRom(base.points, this.resolution, this.closeLoop));
+    }
+}
